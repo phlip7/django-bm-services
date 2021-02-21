4fd62f2679ed10492851127ad33a762c9e22f6b6
phlp <phlp@phlps-MacBook-Pro.local>
2021-02-20 23:06:17 -0500

    install template

--- app/views.py
+++ app/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import CreateUserForm
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    '''if request.user.is_authenticate:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Compte créé pour '+ user)
+                return redirect('login')
+        
+        context = {'form': form}'''
+    return render(request, 'signup.html')
+
+def login(request):
+	'''if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('home')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
+    '''
+	context = {}
+	return render(request, 'login.html', context)
