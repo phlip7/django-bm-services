c382813c391a68d69e1b687205c175356508e1f7
dheroying <dheroying@yandex.com>
2021-05-18 02:58:09 +0800

    change db setting and pip

--- baramogo/settings.py
+++ baramogo/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'baramogo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'baramogo',
+        'USER': 'dhero',
+        'PASSWORD': '1234567D',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
