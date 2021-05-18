4262a930dbe6446ef3143b8a8c395f90fa321067
dheroying <dheroying@yandex.com>
2021-05-18 21:39:51 +0800

    add static folder root

--- baramogo/settings.py
+++ baramogo/settings.py
@@ -31,7 +31,6 @@ DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['localhost', '143.110.214.23']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -74,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'baramogo.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -90,7 +88,6 @@ WSGI_APPLICATION = 'baramogo.wsgi.application'
 # }
 
 DATABASES['default'] = dj_database_url.parse(env('DATABASE_URL'))
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -110,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -124,12 +120,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Setup upload directory for Gig model
-MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
